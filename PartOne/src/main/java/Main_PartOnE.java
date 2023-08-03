295db84bbe49cd05e2c4f54705e28d3d938e8de9
ANK <kalupahanaadheetha5@gmail.com>
2023-08-03 22:58:42 +0530

    update an existing book's attributes(edition)         & save the changes to the database

--- PartOne/src/main/java/Main_PartOnE.java
+++ PartOne/src/main/java/Main_PartOnE.java
@@ -21,10 +21,25 @@ public class Main_PartOnE {
         transaction.commit();
         session.close();
 
-         */
         //retrieve a book by its ID from the database
         Book book = null;
         book = (Book) session.get(Book.class, "9780545069670");
         System.out.println(book);
+
+         */
+
+        //updating an existing book
+        Book book = new Book();
+        book.setISBN("9780545069670");
+        book.setAuthno("J. K. Rowling");
+        book.setTitle("Harry Potter and the Philosopher's Stone");
+        book.setEdition("4");
+        book.setCategory("fantasy literature");
+        book.setPrice(2750.00);
+        session.update(book);
+
+        Book book1 = null;
+        book1 = (Book) session.get(Book.class, "9780545069670");
+        System.out.println(book1);
     }
 }
