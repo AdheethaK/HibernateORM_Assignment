bed869d9830160bacf81c5123910d48121b87946
ANK <kalupahanaadheetha5@gmail.com>
2023-08-03 23:04:54 +0530

    delete a book from the database

--- PartOne/src/main/java/Main_PartOnE.java
+++ PartOne/src/main/java/Main_PartOnE.java
@@ -26,17 +26,21 @@ public class Main_PartOnE {
         book = (Book) session.get(Book.class, "9780545069670");
         System.out.println(book);
 
-         */
-
         //updating an existing book
         Book book = new Book();
         book.setISBN("9780545069670");
-        book.setAuthno("J. K. Rowling");
-        book.setTitle("Harry Potter and the Philosopher's Stone");
-        book.setEdition("4");
-        book.setCategory("fantasy literature");
-        book.setPrice(2750.00);
         session.update(book);
+
+        Book book1 = null;
+        book1 = (Book) session.get(Book.class, "9780545069670");
+        System.out.println(book1);
+
+         */
+
+        //deleting an existing book
+        Book book = new Book();
+        book.setISBN("9780545069670");
+        session.delete(book);
 
         Book book1 = null;
         book1 = (Book) session.get(Book.class, "9780545069670");
