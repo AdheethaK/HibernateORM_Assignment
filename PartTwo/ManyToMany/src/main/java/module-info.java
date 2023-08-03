59b99db512b3a6caa52b2d352ff4fe2ad2b7ac03
ANK <kalupahanaadheetha5@gmail.com>
2023-08-04 02:22:56 +0530

    create Many-to-Many Relationship: each book can have multiple authors, & each author can have multiple books

--- PartTwo/ManyToMany/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.manytomany {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.manytomany to javafx.fxml;
-    exports com.example.manytomany;
-}
